7950017624066d28bfef903378e37a4ab472732a
Abhi <meabhishek1512@gmail.com>
2024-03-23 16:10:48 +0530

    create-product route endpoint

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@ import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
-import userRoutes from "./routes/userRoutes.js"
-import categoryRoutes from "./routes/categoryRoutes.js"
 
 // Utiles
 import connectDB from "./config/db.js"
+import userRoutes from "./routes/userRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js"
+import productRoutes from "./routes/productRoutes.js"
 
 dotenv.config()
 const port = process.env.PORT || 5000
@@ -22,5 +23,6 @@ app.use(cookieParser())
 
 app.use("/api/users", userRoutes)
 app.use("/api/category", categoryRoutes)
+app.use("/api/products", productRoutes)
 
 app.listen(port, () => console.log(`Server running on port: ${port}`))
