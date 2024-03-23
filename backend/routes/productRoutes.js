45b3a3d56c8099ff09454b7580293d2617126b47
Abhi <meabhishek1512@gmail.com>
2024-03-23 18:54:00 +0530

    fetch-product route endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,11 +7,15 @@ import {
     addProduct,
     updateProductDetails,
     removeProduct,
+    fetchProducts,
 } from "../controllers/productController.js"
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js"
 import checkId from "../middlewares/checkId.js"
 
-router.route("/").post(authenticate, authorizeAdmin, formidable(), addProduct)
+router
+    .route("/")
+    .get(fetchProducts)
+    .post(authenticate, authorizeAdmin, formidable(), addProduct)
 
 router
     .route("/:id")
