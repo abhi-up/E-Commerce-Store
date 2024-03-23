7b5aa65e847d59021988894fd303e55ddc51a698
Abhi <meabhishek1512@gmail.com>
2024-03-23 09:11:37 +0530

    admin update-user route endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,6 +8,7 @@ import {
     updateCurrentUserProfile,
     deleteUserById,
     getUserById,
+    updateUserById,
 } from "../controllers/userController.js"
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js"
 
@@ -31,5 +32,6 @@ router
     .route("/:id")
     .delete(authenticate, authorizeAdmin, deleteUserById)
     .get(authenticate, authorizeAdmin, getUserById)
+    .put(authenticate, authorizeAdmin, updateUserById)
 
 export default router
