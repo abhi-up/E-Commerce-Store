007b7fde344bf57fff01e6dc57f163faae9f55df
Abhi <meabhishek1512@gmail.com>
2024-03-23 14:12:37 +0530

    navigation checkpoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import { Route, RouterProvider, createRoutesFromElements } from "react-router"
+import { createBrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./redux/store"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import PrivateRoute from "./components/PrivateRoute"
+
+// Auth
+import Login from "./pages/Auth/Login"
+import Register from "./pages/Auth/Register"
+
+import Profile from "./pages/User/Profile"
+
+import AdminRoute from "./pages/Admin/AdminRoute"
+import UserList from "./pages/Admin/UserList"
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+
+            {/* Registered users */}
+            <Route path="" element={<PrivateRoute />}>
+                <Route path="/profile" element={<Profile />} />
+            </Route>
+
+            {/* Admin Routes */}
+            <Route path="/admin" element={<AdminRoute />}>
+                <Route path="userlist" element={<UserList />} />
+            </Route>
+        </Route>
+    )
+)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
 )
